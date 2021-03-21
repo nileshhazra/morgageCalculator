d54b25bb604c9e384b7ad9b88a6ea8f4e2e89286
nileshhazra <nileshhz99@gmail.com>
2021-03-21 12:24:00 +0530

    Initial commit

---
+++ src/com/nileshhazra/Main.java
@@ -0,0 +1,13 @@
+package com.nileshhazra;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myAge = 30;
+	    int hisAge = 40;
+	    String name = "Nilesh";
+        System.out.println("Hello " + name);
+        System.out.println(myAge);
+        System.out.println(hisAge);
+    }
+}
